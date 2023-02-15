d3c8de05035de780d824bba3d29c11ee2baf095d
Nanashi <alksndr.zln@gmail.com>
2023-02-15 23:47:25 +0300

    Added create_intent.py for uploading questions and answers in Json

---
+++ create_intent.py
@@ -0,0 +1,41 @@
+import os
+import dotenv
+import requests
+from google.cloud import dialogflow
+
+dotenv.load_dotenv()
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+
+response = requests.get(os.environ['URL_FOR_QUESTIONS'])
+response.raise_for_status()
+data = response.json()
+
+for i, j in zip(data.keys(), data.values()):
+    questions = j['questions']
+    answer = j['answer']
+    topic = i
+
+    create_intent(os.environ['PROJECT_ID'], topic, questions, [answer])
